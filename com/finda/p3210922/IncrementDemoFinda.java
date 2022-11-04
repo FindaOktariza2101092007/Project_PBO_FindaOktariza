37f2b29248c607e1819dfc6c4d36aa4c079e9386
FindaOktariza2101092007 <114201253+FindaOktariza2101092007@users.noreply.github.com>
2022-11-04 09:43:33 +0700

    Pertemuan 2 Modul 4

---
+++ com/finda/p3210922/IncrementDemoFinda.java
@@ -0,0 +1,24 @@
+package com.finda.p3210922;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class IncrementDemoFinda {
+    public static void main(String[] args){
+        int i = 10;
+        int j = 3;
+        int k = 0;
+        
+        k = ++j + i;
+        
+        System.out.println("k :"+k);
+        System.out.println("j :"+j);
+    }
+}
