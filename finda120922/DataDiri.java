374d57cf3eab60556e5900e6ddaf259e2d7051c2
FindaOktariza2101092007 <114201253+FindaOktariza2101092007@users.noreply.github.com>
2022-11-04 09:36:22 +0700

    Pertemuan 1 Data diri dan Pengenalan Pbo Dasar

---
+++ finda120922/DataDiri.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finda120922;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class DataDiri {
+// Nama    : 
+// No BP   : 
+// Prodi   : 
+// Jurusan : 
+// Alamat  : 
+    
+    public static void main(String[] args) {
+        System.out.println("Nama\t:Finda Oktariza\nNo BP\t:2101092007\nProdi\t:D3 Manajemen Informatika\nJurusan\t:Teknologi Informasi\nAlamat\t:Pasaman\n");
+    }
+   
+  
+   
+}
---
+++ finda120922/HelloWord.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finda120922;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello World\n"+"Pemrograman Berbasis Objek");
+       // System.out.println("Pemrograman Berbasis Objek");
+                
+    }
+    
+}
